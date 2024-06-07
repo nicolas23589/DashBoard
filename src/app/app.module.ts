3d6efb9a9596ad079f5a1c0df086827bf1f49815
Nicolás23589 <n.murilloc@uniandes.edu.co>
2024-06-07 13:52:37 -0400

    iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxChartsModule,
+    MatCardModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+
+//This Part of the code will be like the "Object" that can be accesible in the dahsboard.component.html
+export class DashboardComponent {
+
+
+  /*The following is the necessary data for every graphic, the most important is that every variable which have data-
+   to be displayed needs to be type any[]*/
+
+  //Necessary data for voltage
+  VoltageValue: any[] = [
+    {
+      "name": "Voltage",
+      "value": 50
+    }];
+  VoltageUnits: string = 'Volts';
+  
+  //Necessary data for Current
+  CurrentValue: any[] = [
+    {
+      "name": "Current",
+      "value": 10
+    }];
+  CurrentUnits: string = 'Ampers';
+
+  //Necessary data for Historic Voltage, 
+  HistoricVoltage: any[] = [
+    {
+      "name": "Voltage",
+      "series": [
+        {
+          "name": "12:00",
+          "value": 0
+        },
+        {
+          "name": "13:00",
+          "value": 3
+        },
+        {
+          "name": "14:00",
+          "value": 4
+        },
+        {
+          "name": "15:00",
+          "value": 2
+        },
+        {
+          "name": "16:00",
+          "value": 10
+        }
+      ]
+    }
+  ];
+  xAxisLabel: string = 'Hour';
+  yAxisLabel: string = 'Voltage';
+
+
+  HistoricCurrent: any[] = [
+    {
+      "name": "Current",
+      "series": [
+        {
+          "name": "12:00",
+          "value": 0
+        },
+        {
+          "name": "13:00",
+          "value": 13
+        },
+        {
+          "name": "14:00",
+          "value": 6
+        },
+        {
+          "name": "15:00",
+          "value": 10
+        },
+        {
+          "name": "16:00",
+          "value": 20
+        }
+      ]
+    }
+  ];
+  xAxisLabelCurrent: string = 'Hour';
+  yAxisLabelCurrent: string = 'current';
+
+}
+
+
+
+
+
